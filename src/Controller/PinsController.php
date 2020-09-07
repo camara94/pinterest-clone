ac9129f4ac7d0994539f02f0417df696a95787b5
camara94 <ldamaro98@gmail.com>
2020-09-07 22:12:21 +0200

    Traitement des formulaires en symfony avec la methode naive

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -1,31 +1,40 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Pin;
 use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class PinsController extends AbstractController
 {
-    private $repos;
-    public function __construct(PinRepository $repos)
-    {
-       $this->repos = $repos; 
-    }
 
     /**
      * @Route("/")
      */
-    public function index(): Response
+    public function index(PinRepository $repos): Response
     {
-        return $this->render('pins/index.html.twig', ['pins'=>$this->repos->findAll()]);
+        return $this->render('pins/index.html.twig', ['pins'=>$repos->findAll()]);
     }
 
-    public function home()
-    {
-        return $this->render('pins/home.html.twig');
+    /**
+     * @Route("/pin/create", methods={"GET","POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em)
+    { 
+        if( $request->isMethod('POST')  ) {
+            $data = $request->request->all();
+            $pin = new Pin;
+            $pin->setTitle($data['title']);
+            $pin->setDescription($data['description']);
+            $em->persist($pin);
+            $em->flush();
+            return $this->redirect('/');
+        }
+        return $this->render('pins/create.html.twig');
     }
 }
